f735d261af6656f4fc226835810122bd9fd5a7e2
TommmyKelly <tommy_kelly@icloud.com>
2021-02-23 20:09:50 +0000

    host

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-const publicPath = path.join(__dirname, "..", "public");
 const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, "..", "public");
 app.use(express.static(publicPath));
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
 
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("public"));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "public", "..", "index.html"))
-  );
-}
 app.listen(port, () => {
-  console.log("Server is up!");
+  console.log(`Server is up on port ${port}!`);
 });
