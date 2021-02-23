d61ec80c65cc0954c3cfe3921ace2d47e6362c66
TommmyKelly <tommy_kelly@icloud.com>
2021-02-23 20:06:00 +0000

    host

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ app.use(express.static(publicPath));
 app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
+
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("public"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "public", "..", "index.html"))
+  );
+}
 app.listen(port, () => {
   console.log("Server is up!");
 });
